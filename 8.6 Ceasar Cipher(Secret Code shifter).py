7a02395ca05732f1bb222265e42eb03974439e9a
Aniket Patil <55218425+aniketpatil03@users.noreply.github.com>
2021-07-01 09:52:53 +0530

    Add files via upload
    This is CEASAR CIPHER code that encodes and decodes a text input by user , with specified shift value

---
+++ 8.6 Ceasar Cipher(Secret Code shifter).py
@@ -0,0 +1,60 @@
+# active recall - logic and meaning?
+
+
+# Double list of alphabets will ensure shift is continued for last alphabets also
+# alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+#             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+#             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+#             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+#             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+#
+# def encrypt(plain_text,shift_amount):
+#     cipher_text = ""
+#     for letter in plain_text:
+#         position = alphabet.index(letter)
+#         new_position = position + shift_amount
+#         cipher_text += alphabet[new_position]
+#     print(f"the encrypted msg text is {cipher_text}")
+#
+# def decrypt(coded_text,shift_amount):
+#     plain_text = ""
+#     for letter in coded_text:
+#         position = alphabet.index(letter)
+#         new_position = position - shift_amount
+#         plain_text += alphabet[new_position]
+#     print(f"The decrypted text is :{plain_text}")
+#
+# display_msg = input("Encrpt or Decrypt")
+# if display_msg.lower() == "encrypt":
+#     text = input("Enter text ")
+#     shift = int(input("Enter shift number "))
+#     encrypt(plain_text=text,shift_amount=shift)
+# else:
+#     text = input("Enter text to decode: ")
+#     shift = int(input("Enter shift value: "))
+#     decrypt(coded_text=text,shift_amount=shift)
+# ########
+##Combined both decode and encode function
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+            'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Encode or Decode: \n")
+text = input("Enter text\n").lower()
+shift = int(input("Shift no: "))
+
+
+def ceasar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+
+    if direction == "decode":
+        shift_amount *= -1
+    for letter in start_text:
+        position = alphabet.index(letter)
+        new_position = position + shift_amount
+        end_text += alphabet[new_position]
+    print(f"the {direction}d msg is {end_text}")
+
+
+ceasar(text, shift, direction)
